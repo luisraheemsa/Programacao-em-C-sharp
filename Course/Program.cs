147a3bb977c8ee2cd3f3c8061905b1baceea99be
Lu <luisraheemsa@gmail.com>
2025-03-24 13:09:46 -0400

    Recapitulando LP usando C-sharp

---
+++ Course/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 100;
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            Console.WriteLine(x);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            System.Console.WriteLine(n4);
+            
+
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // Referencia para o namespace chamado System para usar funcoes
+
+namespace HelloWorld 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // Esta funcao pertence  ao namespace System
+        }
+    }
+}
